600eae9f13f2123ff2ed0bda824d14258a511dad
KrylovaMarinaa <marina2002bls@yandex.ru>
2023-12-15 23:38:00 +0400

    AddGameCommitOne
    add coins

--- Assets/Scenes/MainGameScene/Scripts/Player.cs
+++ Assets/Scenes/MainGameScene/Scripts/Player.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public static Player MainPlayer;
     public TransitionObj obj;
+
+    public int coins = 0;
+    public Text coinsText;
 
     private void Awake()
     {
@@ -23,5 +27,16 @@ public class Player : MonoBehaviour
         obj.PlayHide();
         yield return new WaitForSeconds(2);
         SceneManager.LoadScene(1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            coins++;
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+            coinsText.text = coins.ToString();
+        }
     }
 }
